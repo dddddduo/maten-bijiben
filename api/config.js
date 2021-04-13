d9a92d3dddcfcd16fac5435142cfa5cf6da246af
leiyunduo <1810856114@qq.com>
2021-04-13 09:45:46 +0800

    更新

--- api/config.js
+++ api/config.js
@@ -2,7 +2,7 @@ let baseurl = ""
 
 if(process.env.NODE_ENV === 'development'){
     // 开发环境
-    baseurl = 'http://mkmngsys.mitech-ndt.com/'
+    baseurl = 'https://mkmngsys.mitech-ndt.com/'
 }else{
     // 生产环境
     baseurl = 'https://mkmngsys.mitech-ndt.com/'
